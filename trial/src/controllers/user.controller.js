1fcdb29868c27898afb9fb0ce540debf830b28d1
[soumashree das] <[soumshreedas2004@gmail.com]>
2024-08-16 23:17:37 +0530

    fic the issues

--- trial/src/controllers/user.controller.js
+++ trial/src/controllers/user.controller.js
@@ -4,19 +4,19 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler( async (req,res) => {
-    //get user details from frontend 
-    const { username, name, password, gender, address, pincode, email, phoneNumber,coverImage } = req.body;
+// const registerUser = asyncHandler( async (req,res) => {
+//     //get user details from frontend 
+//     const { username, name, password, gender, address, pincode, email, phoneNumber,coverImage } = req.body;
     
-    //check if user already exist - check user name and emai existence
-    //cover image - if available upload to cloudinary
-    //create user object - create entry in db
-    //remove password and refresh token field
-    //check for user creattion
-    //return res
-})
+//     //check if user already exist - check user name and emai existence
+//     //cover image - if available upload to cloudinary
+//     //create user object - create entry in db
+//     //remove password and refresh token field
+//     //check for user creattion
+//     //return res
+// })
 
-export {registerUser}
+// export {registerUser}
 
 // Controller to add a new user
 const addUser = asyncHandler(async (req, res) => {
@@ -32,26 +32,143 @@ const addUser = asyncHandler(async (req, res) => {
         email,
         phoneNumber,
     });
-    // //validate user - if any field is empty
-    // if(
-    //     [name , username , password , email].some( (field) => {
-    //     field?.trim === ""
-    //     })
-    // ){
-    //     throw new ApiError(400,"all fields are required!")
-    // }
 
-    // //check if the user already exists
-    // const existedUser = User.findOne({
-    //     $or : [ { username } , { email }]
-    // })
-    // if(existedUser) {
-    //     throw new ApiError(409 , "Username or email already exists")
-    // }
+
+
+    //notworking
+    //validate user - if any field is empty
+    if(
+        [username, name, password, gender, address, pincode, email, phoneNumber].some( (field) => {
+        field?.trim() === ""
+        })
+    ){
+        throw new ApiError(400,"name,username,password,email are required!")
+    }
+
+
+
+
+
+
+    //check if the user already exists
+    const existedUser = await User.findOne({ 
+        $or : [ { username } , { name }]
+    })
+    if(existedUser) {
+        throw new ApiError(409 , "Username or email already exists")
+    }
 
     await newUser.save();
+
+    const createdUser = await User.findById(newUser._id).select(
+        "-password -refreshtoken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
     res.status(201).json(new ApiResponse("User added successfully", newUser));
 });
+
+
+
+//controller to login user a registered user
+const loginUser = asyncHandler(async(req,res) => {
+    //req body -> data
+    const {username,email,password} = req.body;
+    if(!username && !email){
+        throw new ApiError(400,"username or email is required!")
+    }
+    //username or email - use either one to find the user
+    const user = await User.findOne({
+        $or : [{username},{email}] // the $or operator helps in passing an array of objects fron which we can check the existence of any one to see if th euser is present or not
+    })//here User is used because User is a mongodb model and the findone is a mongodb method
+
+    if(!user){
+        throw new ApiError(404,"user does not exist!");
+    }
+
+
+    //password check
+    // isPasswordCorrect method is created by me and hence is accesible by my user which is saved as "user" with a "small u"
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(401,"Password is incorrect!");
+    }
+
+    //access or refresh token
+
+    //since we might use the access and refresh token amny times and hence many time it will be rewquierd to be geenrated .. we need are making method which we can access any time
+    const generateAccessandRefreshToken = async(userId) =>{
+        try{
+            await User.findById(userId);
+            const accesstoken = user.generateAccessToken();//this is providded to the user
+
+            const refreshtoken = user.generateRefreshToken();///this is stored in the database so that we do not have to ask th euser again and again for the password
+            user.refreshtoken = refreshtoken;
+            await user.save({validateBeforeSave : false})
+
+            return { accesstoken,refreshtoken };
+
+        }catch(error){
+            throw new ApiError(500,"something went wron gwhile generating refresh and access token!");
+        }
+    }
+    const {accesstoken,refreshtoken} = await generateAccessandRefreshToken(user._id)
+
+    const lopgedInUser = await User.findById(user._id).select("-password -refreshtoken");
+
+    const options = {
+        httpOnly : true,
+        secure : true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accesstoken" , accesstoken,options)
+    .cookies("refreshtoken",refreshtoken,options)
+    .json(
+        new ApiResponse(
+            200,{
+            user : loggedInUser , accesstoken,
+            refreshtoken
+            },
+            "User Logged In Successfully!"
+        )
+    )
+    //send cookies
+})
+
+const logOutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:{
+                refreshtoken : 1//removes the field from doc
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+
+
+
 
 // Controller to get details of a specific user by ID
 const getUserDetails = asyncHandler(async (req, res) => {
@@ -118,8 +235,10 @@ const deleteUser = asyncHandler(async (req, res) => {
 
 export {
     addUser,
+    loginUser,
+    logOutUser,
     getUserDetails,
     getAllUsers,
     updateUser,
-    deleteUser,
+    deleteUser
 };
