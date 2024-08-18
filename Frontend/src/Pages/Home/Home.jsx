2678980bceec8cee4d9fb550e600b919580f811f
Aniket Dey <deyaniket48@gmail.com>
2024-08-18 09:18:02 +0530

    Many file structures changed

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import Featured_products from '../../Components/Productsonhome/Featured_products'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer';
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <Navbar/>
       <Header/>
       <Featured_products/>
-    </div>
+      <Footer/>
+    </>
   )
 }
 
